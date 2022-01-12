80223aa62500855cb8b01597b61b76814d93924f
Dave <david@afixia.com>
2022-01-12 15:59:49 -0600

    Added all possible includes for bedrock features

--- basic_bedrock_build/src/js/app.js
+++ basic_bedrock_build/src/js/app.js
@@ -1,2 +1,29 @@
+// Import Options:
+
+// FRONT END
+// import 'concretecms-bedrock/assets/account/js/frontend'
 // import 'concretecms-bedrock/assets/bedrock/js/frontend'
-// import 'concretecms-bedrock/assets/calendar/js/frontend'; // Example of how to import an individual component
+// import 'concretecms-bedrock/assets/boards/js/frontend'
+// import 'concretecms-bedrock/assets/calendar/js/frontend'
+// import 'concretecms-bedrock/assets/conversations/js/frontend'
+// import 'concretecms-bedrock/assets/desktop/js/frontend'
+// import 'concretecms-bedrock/assets/documents/js/frontend'
+// import 'concretecms-bedrock/assets/express/js/frontend'
+// import 'concretecms-bedrock/assets/express/js/frontend'
+// import 'concretecms-bedrock/assets/imagery/js/frontend'
+// import 'concretecms-bedrock/assets/maps/js/frontend'
+// import 'concretecms-bedrock/assets/multilingual/js/frontend'
+// import 'concretecms-bedrock/assets/navigation/js/frontend'
+
+
+// BACK  END (Not sure why you would need these)
+// import 'concretecms-bedrock/assets/boards/js/backend'
+// import 'concretecms-bedrock/assets/calendar/js/backend'
+// import 'concretecms-bedrock/assets/calendar/js/backend'
+
+
+// OTHER
+// import 'concretecms-bedrock/assets/ckeditor/js/concrete'
+
+
+// NOTE: There are some in SCSS that are not here as they do not have or need JavaScript
